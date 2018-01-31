32869deceb89d9c9b5df0d23bdea93947ee67649
manpreet <sidhumanu737@gmail.com>
2018-01-31 11:07:21 +0530

    First commit Jbehave  upload

---
+++ jbehaveDemo/src/main/java/com/java/jbehave/jbehaveDemo/Account.java
@@ -0,0 +1,23 @@
+package com.java.jbehave.jbehaveDemo;
+
+public class Account {
+	private int balance;
+
+    public Account(int balance) {
+        this.balance = balance;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public int getMoney(int amount) {
+        if (balance < amount)
+            return 0;
+
+        balance = balance - amount;
+
+        return amount;
+
+    }
+}
